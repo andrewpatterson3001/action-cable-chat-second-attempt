418386343741f2312bf4cd3f99f44349a1805964
Andrew Patterson <andrew.patterson.3001@gmail.com>
2016-02-15 14:10:19 -0800

    restart from auth setup

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+
+  root to: 'welcome#index'
+
+  get '/signup' => 'users#new'
+  get '/users' => 'users#create'
+
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Serve websocket cable requests in-process
+  # mount ActionCable.server => '/cable'
+end
