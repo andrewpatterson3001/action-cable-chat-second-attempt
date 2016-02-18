f73e6b8f4405ac91389b1c79949d9be363f9c0d4
Andrew Patterson <andrew.patterson.3001@gmail.com>
2016-02-17 16:18:21 -0800

    in javascripts/cable.coffee give the createConsumer method '/cable' and move the mount line in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => '/cable'
 
   root to: 'welcome#index'
 
@@ -16,5 +17,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
-  mount ActionCable.server => '/cable'
 end
