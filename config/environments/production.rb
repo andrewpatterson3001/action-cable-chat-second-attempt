44d1933c45813045a8f3376b5a053f3df628f946
Andrew Patterson <andrew.patterson.3001@gmail.com>
2016-02-22 01:39:03 -0800

    try fix from https://github.com/reactjs/react-rails/issues/443#issuecomment-180544359

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
   config.react.server_renderer_timeout    ||= 20 # seconds
   config.react.server_renderer = React::ServerRendering::SprocketsRenderer
   config.react.server_renderer_options = {
-    files: ["react-server.js","components.js"], # files to load for prerendering
+    files: ["components.js"], # files to load for prerendering
     replay_console: true,                 # if true, console.* will be replayed client-side
   }
 
